af26e6742167ffd8e72ff90ec9986a15d52fe641
fionamatters <fionalmatters@gmail.com>
2016-04-23 13:20:58 +0100

    Update: Solved twoStrings

---
+++ src/Section3/TwoStrings.java
@@ -0,0 +1,7 @@
+package Section3;
+
+/**
+ * Created by FI on 23-Apr-16.
+ */
+public class TwoStrings {
+}
---
+++ src/Tests/Section3/TwoStringsTest.java
@@ -0,0 +1,16 @@
+package Section3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by FI on 23-Apr-16.
+ */
+public class TwoStringsTest {
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
